9490212df38f5b3bfb2a3619cedcc81efb615cfd
Thaj Anaz <anaz.thaj@gmail.com>
2023-04-18 16:42:09 +0530

    refactor: move the article part of the page to resources/posts/

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -10,18 +10,8 @@
 
 <body>
     <article>
-        <!-- <h1><a href="/post">My First Post</a></h1> -->
-        <!-- <p> -->
-        <!--     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum -->
-        <!--     harum pariatur laboriosam animi ad veniam maiores fuga -->
-        <!--     laudantium provident eius qui, recusandae laborum possimus, eos -->
-        <!--     perferendis ullam unde minus iusto eligendi, ea repellendus -->
-        <!--     commodi. Ipsum, fuga eaque atque amet, veritatis aut vero at -->
-        <!--     error est laborum nobis nisi qui ducimus. -->
-        <!-- </p> -->
         <?= $post; ?>
     </article>
-    <h1>helee</h1>
 
     <a href="/">Go Back</a>
 </body>
