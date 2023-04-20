e45bcf0211a6ac38bb0d181850883eb5cf4da7b1
Thaj Anaz <anaz.thaj@gmail.com>
2023-04-20 23:19:10 +0530

    add: filepath parser

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,12 +18,15 @@ use Illuminate\Support\Facades\Route;
 Route::get(
     '/',
     function () {
-        return view(
-            'posts',
-            [
-            'posts' => Post::all()
-            ]
-        );
+        $path = resource_path('posts/my-fourth-post.html');
+        $document = YamlFrontMatter::parseFile($path);
+        ddd($document);
+        // return view(
+        //     'posts',
+        //     [
+        //     'posts' => Post::all()
+        //     ]
+        // );
     }
 );
 
